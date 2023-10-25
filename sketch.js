6adcb23a052edd730eeef5b63bfdc499caf7e6b4
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-10-25 18:02:51 +1100

    preload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+var song;
+
+//Preload the mp3 file to the 'song'varible
+function preload(){
+  song = loadSound('Asset/Abel Korzeniowski - Satin Birds.mp3');
+}
+
 function setup() {
   createCanvas(400, 400);
 }

f787b772ac131ed7e58a9f0fcd7a335416e92392
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-10-25 18:39:45 +1100

    circle wave

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
   fft = new p5.FFT();
 }
 
@@ -16,20 +17,28 @@ function draw() {
   stroke(255);
   noFill();
   //Store the fft data
+
+  translate(width / 2, height / 2); //Move to the center of the screen
+
+
   var wave = fft.waveform();
 
-  //Use beginshape and endshape to make all points connected by a line
-  beginShape();
-  //Looping through the waveform data to create the wave on canva
-  for (let i = 0; i < width; i++) {
-    //Use floor to make sure the mapped value is an integer
-    var index = floor(map(i, 0, width, 0, wave.length));
-    var x = i;
-    //y coordinator is the current waveform value based on index
-    var y = wave[index] * 300 + height / 2; //Scale it up to better see it, also offset the satring point down
-    vertex(x, y);
+  //Instead of draw two half circle, use a loop function
+  for (var t = -1; t <= 1; t += 2) { //t needs to be -1 and 1
+
+    //Use beginshape and endshape to make all points connected by a line
+    beginShape();
+    //Looping through the waveform data to create the wave on canva
+    for (let i = 0; i <= 180; i += 0.5) { //180 indicates for 180 degrees, which is the half circle
+      //Use floor to make sure the mapped value is an integer
+      var index = floor(map(i, 0, 180, 0, wave.length - 1));
+      var r = map(wave[index], -1, 1, height / 6, height / 3); //The last two arguments is the min and max radius, the 2nd and 3rd ctrl the amplitude
+      var x = r * sin(i) * t; //When t is -1, it draws the left half and 1 for the right half
+      var y = r * cos(i);
+      vertex(x, y);
+    }
+    endShape();
   }
-  endShape();
 }
 
 //A function to let user use mouse click to play the song

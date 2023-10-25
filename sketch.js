4b58d5d9fbe89752269210a5d2de1527bc1b2d16
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-10-25 18:22:02 +1100

    vertex

--- sketch.js
+++ sketch.js
@@ -12,20 +12,24 @@ function setup() {
 }
 
 function draw() {
-  background(255);
-  stroke(0);
+  background(0);
+  stroke(255);
+  noFill();
   //Store the fft data
   var wave = fft.waveform();
 
+  //Use beginshape and endshape to make all points connected by a line
+  beginShape();
   //Looping through the waveform data to create the wave on canva
   for (let i = 0; i < width; i++) {
     //Use floor to make sure the mapped value is an integer
     var index = floor(map(i, 0, width, 0, wave.length));
     var x = i;
     //y coordinator is the current waveform value based on index
-    var y = wave[index] * height / 4 + height / 2; //Scale it up to better see it, also offset the satring point down
-    point(x, y);
+    var y = wave[index] * 300 + height / 2; //Scale it up to better see it, also offset the satring point down
+    vertex(x, y);
   }
+  endShape();
 }
 
 //A function to let user use mouse click to play the song
